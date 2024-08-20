8b8cb68c2e129432cbf4f3ec43b7db6809afd7c6
Attique Minhas <attiqueminhasofficial@gmail.com>
2024-08-19 22:33:00 -0700

    User Functionality completed in User tab Under Dashboard Menu

--- POS.Data/obj/Debug/net6.0/POS.Data.AssemblyInfo.cs
+++ POS.Data/obj/Debug/net6.0/POS.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1dddcb242a45d09db930c1b01aca1b90f9fd82")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
