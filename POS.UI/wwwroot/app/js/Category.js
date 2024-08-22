300b5a39ec0764c3801c88cbebf28d62770cd887
Attique Minhas <attiqueminhasofficial@gmail.com>
2024-08-21 22:53:03 -0700

    Had did changes in category.cshtml file related to datatable and created category.js file

---
+++ POS.UI/wwwroot/app/js/Category.js
@@ -0,0 +1,20 @@
+﻿/*const { post } = require("jquery");*/
+
+$(document).ready(function () {
+    $('#myCategoryTable').dataTable({
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        scrollX: true,
+        scrollY: true,
+        "dom": 'Bfrtip', // Position of the buttons
+        "buttons": [
+            {
+                extend: 'excelHtml5',
+                text: '<i class="fa fa-file-excel"></i> Export to Excel',
+                className: 'btn btn-success btn-export',
+                titleAttr: 'Export to Excel'
+            }
+        ]
+    });
+});
